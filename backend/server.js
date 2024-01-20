eaed1c047625252c348dd7ff85ccb90d0405ca07
Yash <sharmayash2862@gmail.com>
2024-01-20 23:05:54 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,26 @@
 require('./config/db');
+const cors = require('cors');
+const bodyParser = require('express');
 
 const app = require('express')();
 const port = process.env.PORT ||5000;
 
-const cors = require('cors');
 app.use(cors());
 
+app.use(bodyParser.json());
+
+
+
+
 const UserRouter = require('./api/user')
-
-
-
-const bodyParser = require('express').json;
-app.use(bodyParser());
-
 app.use('/user',UserRouter)
 
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  });
 
 
 app.listen(port, ()=>{
