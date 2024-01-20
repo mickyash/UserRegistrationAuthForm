c369b59e89610a4120e1eaa135f6fbe1b8b5abe2
Yash <sharmayash2862@gmail.com>
2024-01-20 23:19:11 +0530

    sever cors update

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,20 @@ const port = process.env.PORT ||5000;
 app.use(cors());
 
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://user-registration-auth-form-e28x.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
 
 
-
-const corsOptions = {
-    origin: 'https://user-registration-auth-form-e28x.vercel.app',
-    methods: 'POST,GET',
-    credentials: true,
-  };
+// const corsOptions = {
+//     origin: 'https://user-registration-auth-form-e28x.vercel.app',
+//     methods: 'POST,GET',
+//     credentials: true,
+//   };
   
   app.use(cors(corsOptions));
 
