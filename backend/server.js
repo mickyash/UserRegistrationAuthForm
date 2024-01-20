640cafe052353963d23ae2b4a9e88cb9f3672639
Yash <sharmayash2862@gmail.com>
2024-01-20 23:36:50 +0530

    sarver update

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
 
 app.use(cors(corsOptions));
 
+app.options('/user/signup', cors(corsOptions)); 
 
 // const corsOptions = {
 //     origin: 'https://user-registration-auth-form-e28x.vercel.app',
